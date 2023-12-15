aec3fd816d43c68bb44250f0bb4e4366d907e145
nameko-ko <nameko.tpert@gmail.com>
2023-12-15 11:23:07 +0900

    add

--- Hello_world.py
+++ Hello_world.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 theta = np.arange(0,2*np.pi,0.1)
 y = np.sin(theta)
 
-plt.scatter(theta,y)
+plt.plot(theta,y)
+plt.xlabel("t /frame")
+plt.tick_params(bottom=False, left=False, right=False, top=False)
 plt.show()
+
